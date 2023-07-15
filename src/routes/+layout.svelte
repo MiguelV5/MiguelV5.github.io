21d5cf54ef48443154b945b710ac957888b024c5
MiguelV5 <mvasquezj@fi.uba.ar>
2023-07-15 03:30:12 -0300

    add base AppShell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,26 @@
 <script>
-	// Your selected Skeleton theme:
+	// selected Skeleton theme:
 	import '../theme.postcss';
 
 	// This contains the bulk of Skeletons required styles:
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 
-	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	// global stylesheet
 	import '../app.postcss';
+
+	// ====================================================
+
+	import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<h1 class="h1">Header</h1>
+	</svelte:fragment>
+
+	<slot />
+
+	<svelte:fragment slot="footer">
+		<p>Footer</p>
+	</svelte:fragment>
+</AppShell>
