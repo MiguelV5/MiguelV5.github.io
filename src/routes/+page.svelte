7bea1e9c363b2dcab331ef566b9d7558934b2d8f
MiguelV5 <mvasquezj@fi.uba.ar>
2023-07-20 18:23:30 -0300

    finished project-card template (warning: its coupled with the bio-card styling)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,9 @@
 	const MIN_WINDOW_WIDTH_FOR_TITLE_CHANGE = 768;
 
 	const BASE_PREVIEW_IMG_URL = '../../posts/';
+	const BASE_GH_URL = 'https://github.com/MiguelV5/';
+	const BASE_PAGES_URL = 'https://miguelv5.github.io/';
+
 	const previewImgsList = [
 		BASE_PREVIEW_IMG_URL + 'polyrythmic_circles.png',
 		BASE_PREVIEW_IMG_URL + 'ferris_torrent.png'
@@ -59,23 +62,38 @@
 
 	<div class="bio-card-container space-y-6">
 		<div class="w-full text-token grid grid-cols-1 md:grid-cols-1 gap-4">
-			<a class="card variant-ghost-success card-hover overflow-hidden" href="#/">
+			<a
+				class="card variant-ghost-success card-hover overflow-hidden"
+				href={BASE_PAGES_URL + 'polyrythmic_circles'}
+				target="_blank"
+				rel="noopener noreferrer"
+			>
 				<header>
 					<img src={previewImgsList[0]} class="bg-black/50" alt="Post" />
 				</header>
 				<div class="p-4 space-y-4">
-					<h3 class="h3" data-toc-ignore>Skeleton is Awesome!</h3>
+					<h3 class="h3" data-toc-ignore>Title</h3>
 					<article>
 						<p>
 							<!-- cspell:disable -->
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet
-							eligendi cumque consequatur tempore sint nisi sapiente. Iste beatae laboriosam iure molestias
-							cum expedita architecto itaque quae rem.
+							Text
 							<!-- cspell:enable -->
 						</p>
 					</article>
 				</div>
-				<hr class="opacity-50" />
+				<footer class="p-4 mt-28 space-x-4">
+					<hr class="opacity-100" />
+					<br />
+					<div class="flex-auto flex justify-end items-center">
+						<a
+							href={BASE_GH_URL + 'polyrythmic_circles'}
+							target="_blank"
+							rel="noopener noreferrer"
+							class="btn variant-ghost-success"
+							>View repository &nbsp; <i class="fa-brands fa-github" /></a
+						>
+					</div>
+				</footer>
 			</a>
 		</div>
 	</div>
@@ -121,7 +139,12 @@
 	.bio-card-container {
 		width: 100%;
 		max-width: 400px;
+		min-height: 600px;
+		max-height: 600px;
 		margin-left: 40px;
+	}
+	.card {
+		min-height: 500px;
 	}
 
 	@media screen and (max-width: 1180px) {
