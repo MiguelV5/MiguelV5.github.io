036ca16363918ff6ff2ae4565f5d48cf73fc7782
MiguelV5 <mvasquezj@fi.uba.ar>
2026-04-29 05:13:30 -0300

    content udpate

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -5,19 +5,36 @@ const BASE_GL_URL = 'https://gitlab.com/MiguelV5/';
 const BASE_PAGES_URL = 'https://miguelv5.github.io/';
 
 export const PROJECTS_LIST = [
+    // {
+    //     title: 'Polyrythmic Circles',
+    //     description:
+    //         'A simple, relaxing static page featuring the polyrythmic effect, done with vanilla JS, HTML and CSS.',
+    //     previewImgUrl: BASE_PREVIEW_IMG_URL + 'polyrythmic_circles.png',
+    //     repoUrl: BASE_GH_URL + 'polyrythmic_circles',
+    //     hasWebPage: true,
+    //     pageUrl: BASE_PAGES_URL + 'polyrythmic_circles'
+    // },
     {
-        title: 'Polyrythmic Circles',
+        title: "RPL 3.0 | Senior Engineering Thesis",
         description:
-            'A relaxing static page featuring the polyrythmic effect, done simply with vanilla JS, HTML and CSS.',
-        previewImgUrl: BASE_PREVIEW_IMG_URL + 'polyrythmic_circles.png',
-        repoUrl: BASE_GH_URL + 'polyrythmic_circles',
+            "Maintained and overhauled an open-source platform serving 5k+ active users and ~400k code-execution submissions per four-month period. Built a sandboxed runner for asynchronous multi-language execution (C, Python, Rust, etc.). Led zero-downtime data migration through multiple MySQL major updates alongside database-per-service schema separation, all whilst preserving legacy records. The system leverages automated GCP (GKE) infrastructure with Terraform and Kubernetes via CI/CD pipelines, including autoscaling and secure inter-service auth (mTLS), as well as a full-stack entrypoint.",
+        previewImgUrl: BASE_PREVIEW_IMG_URL + 'rpl.png',
+        repoUrl: "https://github.com/reinvent-fiuba/RPL-3.0",
         hasWebPage: true,
-        pageUrl: BASE_PAGES_URL + 'polyrythmic_circles'
+        pageUrl: "https://myrpl.ar/"
+    },
+    {
+        title: "Amazon reviews' mass analysis via distributed computing",
+        description:
+            "A distributed system for processing and analyzing large datasets of Amazon books reviews in parallel, built with a fleet of dockerized Python controllers and RabbitMQ as a message broker. It implements a MapReduce-like architecture, where a master node distributes the workload among multiple worker nodes categorized by responsibilities. The system is designed to be scalable and fault-tolerant, with the ability to handle highly unstable networks/environments by properly managing catastrophic failures without losing data (even in scenarios with write-corruption) or interrupting the general processing.",
+        previewImgUrl: BASE_PREVIEW_IMG_URL + 'distri.png',
+        repoUrl: BASE_GH_URL + 'Sistemas_Distribuidos',
+        hasWebPage: false
     },
     {
         title: 'Ferris Torrent',
         description:
-            'A BitTorrent client supporting multiple concurrent peers, and a custom tracker server with a simple static web interface for basic stats of its peers. Both done in Rust while keeping external crate usage to a minimum (TLS, GTK, logging, json) for my Programming Workshop college subject.',
+            'A BitTorrent client supporting multiple concurrent peers, and a custom tracker server with a simple static web interface for basic stats of its peers. Both implemented in Rust while keeping external crate usage to a strict minimum (TLS, GTK, logging, json) in order to properly handle real-world network conditions and adhere to documented protocol specifications without relying on third-party implementations.',
         previewImgUrl: BASE_PREVIEW_IMG_URL + 'ferris_torrent.png',
         repoUrl: BASE_GH_URL + 'Ferris-Torrent_taller1-fiuba',
         hasWebPage: false
@@ -25,7 +42,7 @@ export const PROJECTS_LIST = [
     {
         title: 'FileTransfer + RDT',
         description:
-            'A CLI File Transfer app using a custom RDT protocol on top of UDP, handling packet loss within a simulated network via mininet. Written in python for my Computer Networking college subject. A custom wireshark dissector made in Lua is also provided.',
+            'A CLI File Transfer app implementing a custom RDT protocol on top of UDP, handling packet loss within a simulated network via mininet. Written in python for my Computer Networking college subject. A custom wireshark dissector made in Lua is also provided.',
         previewImgUrl: BASE_PREVIEW_IMG_URL + 'file_transfer.png',
         repoUrl: BASE_GH_URL + '7543_IntroSistDistribuidos/tree/main/tp1',
         hasWebPage: false
@@ -41,26 +58,26 @@ export const PROJECTS_LIST = [
     {
         title: "Properties' pricing analysis",
         description:
-            "A data science project done with jupyter for my Data Science college subject. It consists of the data analysis and model training from a dataset with 2021's properties for sale in Buenos Aires, Argentina. It includes full data preprocessing, clustering, classification and regression; training multiple ML models.",
+            "A data science project consisting of analysis and model training from a dataset with 2021's properties for sale in Buenos Aires, Argentina. It includes full data preprocessing, clustering, classification and regression models, presenting the results and insights obtained from the record exploration and model evaluation with visualizations and metrics.",
         previewImgUrl: BASE_PREVIEW_IMG_URL + 'ds.png',
         repoUrl: BASE_GH_URL + '7506-Organizacion_de_datos',
         hasWebPage: false
     },
-    {
-        title: "Human Resources' Backend API",
-        description:
-            "A basic REST API implementation of a microservice supporting CRUD operations on a Human Resources' local SQLite database, done with Python and FastAPI for my Software Engineering I college subject. It has been deployed on Render cloud platform, and has a simple frontend made with React to interact with all the microservices.",
-        previewImgUrl: BASE_PREVIEW_IMG_URL + 'hr.png',
-        repoUrl: BASE_GH_URL + "7509-ModuloRecursos-squad_6",
-        hasWebPage: true,
-        pageUrl: "https://rrhh-squad6-1c2023.onrender.com/docs"
-    },
-    {
-        title: "Sport Events' Backend API",
-        description:
-            "A REST API for a sport events' management app, done with Java and Spring Boot for my Software Engineering II college subject. It uses JPA/Hibernate as ORM for a PostgreSQL database, and has JWT auth and role-based access control to be used by two frontends: a backoffice for administrators, and a web app for event attendees. It also had a CI/CD pipeline on GitLab, and was deployed on a server given by our college professors.",
-        previewImgUrl: BASE_PREVIEW_IMG_URL + 'sport_events.png',
-        repoUrl: BASE_GH_URL + '7510_TDDSportsApp_API',
-        hasWebPage: false
-    }
+    // {
+    //     title: "Human Resources' Backend API",
+    //     description:
+    //         "A basic REST API implementation of a microservice supporting CRUD operations on a Human Resources' local SQLite database, done with Python and FastAPI for my Software Engineering I college subject. It has been deployed on Render cloud platform, and has a simple frontend made with React to interact with all the microservices.",
+    //     previewImgUrl: BASE_PREVIEW_IMG_URL + 'hr.png',
+    //     repoUrl: BASE_GH_URL + "7509-ModuloRecursos-squad_6",
+    //     hasWebPage: true,
+    //     pageUrl: "https://rrhh-squad6-1c2023.onrender.com/docs"
+    // },
+    // {
+    //     title: "Sport Events' Backend API",
+    //     description:
+    //         "A REST API for a sport events' management app, done with Java and Spring Boot for my Software Engineering II college subject. It uses JPA/Hibernate as ORM for a PostgreSQL database, and has JWT auth and role-based access control to be used by two frontends: a backoffice for administrators, and a web app for event attendees. It also had a CI/CD pipeline on GitLab, and was deployed on a server given by our college professors.",
+    //     previewImgUrl: BASE_PREVIEW_IMG_URL + 'sport_events.png',
+    //     repoUrl: BASE_GH_URL + '7510_TDDSportsApp_API',
+    //     hasWebPage: false
+    // }
 ];
